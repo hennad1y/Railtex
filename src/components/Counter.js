8498551ea1272b4dc30ceaea57ae29dcbb968988
Artemios <d15e3996@protonmail.com>
2020-03-16 15:25:43 +0200

    init

---
+++ src/components/Counter.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import connect from 'react-redux'
+import { increment, decrement } from '../actions/counter'
+
+const Counter = (props) => (
+  <div>
+    Counter: {props.count}
+    <button onClick={props.increment}>+</button>
+    <button onClick={props.decrement}>-</button>
+  </div>
+)
+
+Counter.propTypes = {
+  count: PropTypes.number,
+  increment: PropTypes.func.isRequired,
+  decrement: PropTypes.func.isRequired,
+}
+
+const mapStateToProps = state => ({
+  count: state.count,
+})
+
+const mapDispatchToProps = dispatch => ({
+  increment: () => new Promise((resolve) => {
+      setTimeout(() => {
+          dispatch(increment())
+          resolve()
+      }, Math.random() * 1900 + 100)
+  }),
+  decrement: () => new Promise((resolve) => {
+      setTimeout(() => {
+          dispatch(decrement())
+          resolve()
+      }, Math.random() * 1900 + 100)
+  })
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter)
